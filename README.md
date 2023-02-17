82613f78e5fa6514e28c3164de1139f47ac60374
AlliysonBonner <alliyson.bonner@gmail.com>
2023-02-16 22:27:21 -0600

    Add image, text, and license files to Mad Libs project  This commit adds an image.png file to the project, which is a cartoon-style jungle scene with various animals holding speech bubbles with blank spaces, representing Mad Libs words. It also updates the README.md file to include information about the project, and adds a LICENSE.md file to specify the licensing terms for the code. This commit should make the project more accessible and understandable for other developers and users.

---
+++ README.md
@@ -0,0 +1,29 @@
+# Mad Libs
+![Cartoon jungle scene with various animals holding speech bubbles with blank spaces, representing Mad Libs words. In the foreground, a parrot holds a speech bubble with a blank space. Trees and vines are visible in the background. Colorful and whimsical.](https://github.com/alliysonbonner/MadLib/image.png?raw=true)
+Mad Libs is a word game in which players fill in the blanks of a story with random words, resulting in often humorous and unexpected sentences. This project is a simple implementation of the game in Python.
+
+## Getting Started
+To play Mad Libs, simply run the madlibs.py script and follow the prompts to enter your chosen words. The resulting story will be displayed on the screen.
+
+### Prerequisites
+To run the Mad Libs program, you need to have Python installed on your computer.
+
+### Installing
+1. Clone the Mad Libs Git repository to your local machine.
+1. Navigate to the directory containing the madlibs.py script in your terminal or command prompt.
+1. Run the script by typing python madlibs.py and pressing Enter.
+
+## Usage
+Mad Libs is a fun and simple game that can be played by people of all ages. It is a great way to exercise your creativity and sense of humor. Simply follow the prompts in the command line interface to enter words for the story, and then sit back and enjoy the often hilarious results.
+
+## Contributing
+If you would like to contribute to Mad Libs, you can fork the repository and submit a pull request. Please ensure that your code adheres to the PEP 8 style guide and includes appropriate documentation and unit tests.
+
+## Authors
+* Alliyson Bonner
+
+## License
+This project is licensed under the MIT License - see the [LICENSE.md](https://github.com/alliysonbonner/MadLib/LICENSE.md) file for details.
+
+## Acknowledgments
+* The Mad Libs game was created by Leonard Stern and Roger Price in the 1950s.
--- madlibs.py
+++ madlibs.py
@@ -1,43 +1,29 @@
-# Mad Libs - A player in the word game Mad Libs asks another player for a list of
-# words to fill in the blanks in a story. When the resulting story is read aloud,
-# the word substitutions have a humorous effect. Mad Libs is a play on ad lib,
-# from the Latin ad libitum, which means as you wish.
+def get_input(prompt: str) -> str:
+    """Prompt the user for input and return the input as a string.
 
-# Author: Alliyson Bonner
-# Github: https://github.com/alliysonbonner
-# Linkedin: https://www.linkedin.com/in/alliyson-bonner-6404976b/
-# Email: alliyson.bonner@gmail.com
-# Date: December 07 2022
+    Args:
+        prompt: A string representing the input prompt.
 
-# Prompt user for input and assign varibles
-adjective1 = input("Adjective: ")
-adjective2 = input("Adjective: ")
-adjective3 = input("Adjective: ")
-noun1 = input("Noun: ")
-adjective4 = input("Adjective: ")
-noun2 = input("Noun: ")
-verb1 = input("Verb: ")
-verb2 = input("Verb: ")
-adjective5 = input("Adjective: ")
-noun3 = input("Noun: ")
-verb3 = input("Verb: ")
-noun4 = input("Noun: ")
-verb4 = input("Verb: ")
-adjective6 = input("Adjective: ")
+    Returns:
+        A string representing the user's input.
+    """
+    return input(prompt + ": ")
 
-# Construct madlib string
-madlib = f"I walk through the color jungle. I take out my {adjective1} canteen. \
-There's a {adjective2} parrot with a {adjective3} {noun1} in his mouth right \
-there in front of me {adjective4} {noun2}. A sudden sound awakes me from my \
-daydream! A panther's {verb1} in front of my head! I {verb2} his {adjective5} \
-breath. I remember I have a packet of {noun3} that makes me go into a deep \
-slumber! I {verb3} it away from me in front of the {noun4}. Yes he's eating it! \
-I {verb4} away through the {adjective6} jungle. I meet my parents at the tent. \
+
+# Prompt the user for input and assign variables using the get_input function
+adjectives = [get_input("Adjective") for _ in range(6)]
+nouns = [get_input("Noun") for _ in range(4)]
+verbs = [get_input("Verb") for _ in range(4)]
+
+# Construct madlib string using f-strings
+madlib = f"I walk through the color jungle. I take out my {adjectives[0]} canteen. \
+There's a {adjectives[1]} parrot with a {adjectives[2]} {nouns[0]} in his mouth right \
+there in front of me {adjectives[3]} {nouns[1]}. A sudden sound awakes me from my \
+daydream! A panther's {verbs[0]} in front of my head! I {verbs[1]} his {adjectives[4]} \
+breath. I remember I have a packet of {nouns[2]} that makes me go into a deep \
+slumber! I {verbs[2]} it away from me in front of the {nouns[3]}. Yes he's eating it! \
+I {verbs[3]} away through the {adjectives[5]} jungle. I meet my parents at the tent. \
 Phew! It’s been an exciting day in the jungle."
 
 # Output madlib statement
 print("\n" + madlib)
-
-
-
-
